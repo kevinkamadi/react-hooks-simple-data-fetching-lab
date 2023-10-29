5099646d6353e9cd764f1ee6b72c56e78079e2f6
Your Name <kkamadi567@gmail.com>
2023-10-29 23:04:47 +0300

    added

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import React, { useEffect, useState } from 'react'
+
+function App() {
+
+    const [imageSrc, setImageSrc] = useState('')
+
+    useEffect(() => {
+        fetch(`https://dog.ceo/api/breeds/image/random`)
+        .then(r => r.json())
+        .then(data => setImageSrc(data.message))
+    }, [])
+
+    return (
+        <div>
+            {imageSrc === '' ? <p>Loading...</p> : <img src={imageSrc} alt='A Random Dog' />}
+        </div>
+    )
+}
+
+export default App
